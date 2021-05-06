f2785dcd268dabe5e102e898a1d64b968d17ecf0
github-action-benchmark <github@users.noreply.github.com>
2021-05-06 16:15:50 +0000

    add connect (cargo) benchmark result for d1d2f32a7358c1c7d489ebbb98f4cbfdca9bb573

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1620317749495,
+  "repoUrl": "https://github.com/acfoltzer/hyper",
+  "entries": {
+    "connect": [
+      {
+        "commit": {
+          "author": {
+            "email": "mohsen-alizadeh@users.noreply.github.com",
+            "name": "Mohsen Alizadeh",
+            "username": "mohsen-alizadeh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d1d2f32a7358c1c7d489ebbb98f4cbfdca9bb573",
+          "message": "docs(headers): no_inline doc on HeaderMap (#2525)",
+          "timestamp": "2021-05-03T13:58:34-07:00",
+          "tree_id": "72f2a5e2407fddbc549e5cbc070fc913c857db94",
+          "url": "https://github.com/acfoltzer/hyper/commit/d1d2f32a7358c1c7d489ebbb98f4cbfdca9bb573"
+        },
+        "date": 1620317740571,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http_connector",
+            "value": 61304,
+            "range": "± 6852",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
